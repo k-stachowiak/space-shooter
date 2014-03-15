5fd3126977248ae2f6ceaed9d30eefdc5f8e87eb
k-stachowiak <krzysiek.stachowiak@gmail.com>
2014-03-15 21:10:43 +0100

    updating

--- src/efwk2/cmp/weaponry.h
+++ src/efwk2/cmp/weaponry.h
@@ -59,6 +59,68 @@ public:
 
 SFINAE__DECLARE_HAS_MEMBER(HasPlayerWeapons, player_weapons, pweap);
 
+struct weap_period_bullet
+{
+        cooldown_stat cdown;
+        double x_offset;
+        double y_offset;
+        double y_dir;
+};
+
+SFINAE__DECLARE_HAS_MEMBER(HasPeriodBullet, weap_period_bullet, weap);
+
+struct weap_period_missile
+{
+        cooldown_stat cdown;
+        double x_offset;
+        double y_offset;
+        double y_dir;
+};
+
+SFINAE__DECLARE_HAS_MEMBER(HasPeriodMissile, weap_period_missile, weap);
+
+struct weap_compound_base {};
+
+template <class... Weaps>
+struct weap_compound : public weap_compound_base
+{
+        std::tuple<Weaps...> impl;
+        weap_compound(const Weaps&... new_impl) :
+                impl(new_impl...)
+        {}
+};
+
+template <class Func, int Index, class... Weaps>
+struct cpd_weap_functor
+{
+        void operator()(const weap_compound<Weaps...>& cpd, Func func)
+        {
+                func(Index, std::get<Index>(cpd.impl));
+                cpd_weap_functor<Func, Index - 1, Weaps...> ftor;
+                ftor(cpd, func);
+        }
+};
+
+template <class Func, class... Weaps>
+struct cpd_weap_functor<Func, -1, Weaps...>
+{
+        void operator()(const weap_compound<Weaps...>&, Func) {}
+};
+
+template <class Func, class... Weaps>
+void for_each_weap(const weap_compound<Weaps...>& cpd, Func func)
+{
+        cpd_weap_functor<Func, sizeof...(Weaps) - 1, Weaps...> fctor;
+        fctor(cpd, func);
+}
+
+SFINAE__DECLARE_HAS_MEMBER(HasWeaponCompound, weap_compound_base, weap);
+
+template <class T>
+using HasWeapon = TmpAny<HasPeriodBullet<T>,
+                         HasPeriodMissile<T>,
+                         HasWeaponCompound<T>>;
+
 }
 
 #endif
